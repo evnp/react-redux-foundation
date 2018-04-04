2549774632de752a0262b76833bf56880fbb0fef
Evan Purcer <evanpurcer@gmail.com>
2018-04-04 11:23:31 -0700

    add OSX notifications for webpack builds

--- webpack.config.js
+++ webpack.config.js
@@ -2,92 +2,95 @@ var poststylus = require('poststylus');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var gitCommitHash = require('git-rev-sync');
+var WebpackBuildNotifierPlugin = require('webpack-build-notifier');
 
 function generateBuildFileName(extension) {
-    var hash = gitCommitHash.short(); // use "short" 7 character hash
-    var minified = process.argv.indexOf('-p') !== -1 ||
-                   process.argv.indexOf('--optimize-minimize') !== -1;
-    return [
-        'build',
-        hash,
-        'min',
-        extension,
-    ] // only include .min if we're minifying
-    .filter(function (part) { return minified || part !== 'min'; })
-    .join('.');
+  var hash = gitCommitHash.short(); // use "short" 7 character hash
+  var minified = process.argv.includes('-p') || process.argv.includes('--optimize-minimize');
+  return [
+    'build',
+    hash,
+    'min',
+    extension,
+  ] // only include .min if we're minifying
+  .filter(function (part) { return minified || part !== 'min'; })
+  .join('.');
 }
 
 module.exports = {
-    context: __dirname,
-    cache: true,
-    entry: [
-        'babel-polyfill',
-        __dirname + '/src/js/index.js',
+  context: __dirname,
+  cache: true,
+  entry: [
+    'babel-polyfill',
+    __dirname + '/src/js/index.js',
+  ],
+  output: {
+    filename: generateBuildFileName('js'),
+    path: __dirname + '/build',
+  },
+  module: {
+    loaders: [
+      { // JavaScript Loader - bundle .js import hierarchy (starting at index.js) into a single .js file
+        test: /\.js$/,
+        include: __dirname + '/src/js',
+        loader: 'babel',
+        query: {
+          plugins: [
+            'transform-runtime',
+            'transform-class-properties',
+            'transform-object-rest-spread',
+            'syntax-trailing-function-commas',
+          ],
+          presets: [
+            'es2015',
+            'react',
+          ],
+        }
+      },
+      { // Style Loader - bundle .styl imports in .js files into a single .css file
+        test: /\.styl$/,
+        include: __dirname + '/src/style',
+        loader: process.env.NODE_ENV === 'production' ? // only use the ExtractTextPlugin in production
+          ExtractTextPlugin.extract('style', 'css!stylus') :
+          'style!css!stylus',
+      },
+      { // Image Loader - resolve "url(...)" statements in .css files to images
+        test: /\.(png|jpg|svg)$/,
+        include: __dirname + '/src/images',
+        loader: 'url?limit=25000',
+      },
+      { // JSON Loader - resolve .json imports in .js files to JavaScript objects
+        test: /\.json$/,
+        include: __dirname + '/src/json',
+        loader: 'json',
+      },
+      { // React Hot Loader - automatically refresh page and reload assets on component .js and .styl changes
+        test: /\.(js|styl)$/,
+        include: __dirname + '/src/*/components/',
+        loader: 'react-hot',
+      },
     ],
-    output: {
-        filename: generateBuildFileName('js'),
-        path: __dirname + '/build',
-    },
-    module: {
-        loaders: [
-            { // JavaScript Loader - bundle .js import hierarchy (starting at index.js) into a single .js file
-                test: /\.js$/,
-                include: __dirname + '/src/js',
-                loader: 'babel',
-                query: {
-                    plugins: [
-                        'transform-runtime',
-                        'transform-class-properties',
-                        'transform-object-rest-spread',
-                        'syntax-trailing-function-commas',
-                    ],
-                    presets: [
-                        'es2015',
-                        'react',
-                    ],
-                }
-            },
-            { // Style Loader - bundle .styl imports in .js files into a single .css file
-                test: /\.styl$/,
-                include: __dirname + '/src/style',
-                loader: process.env.NODE_ENV === 'production' ? // only use the ExtractTextPlugin in production
-                    ExtractTextPlugin.extract('style', 'css!stylus') :
-                    'style!css!stylus',
-            },
-            { // Image Loader - resolve "url(...)" statements in .css files to images
-                test: /\.(png|jpg|svg)$/,
-                include: __dirname + '/src/images',
-                loader: 'url?limit=25000',
-            },
-            { // JSON Loader - resolve .json imports in .js files to JavaScript objects
-                test: /\.json$/,
-                include: __dirname + '/src/json',
-                loader: 'json',
-            },
-            { // React Hot Loader - automatically refresh page and reload assets on component .js and .styl changes
-                test: /\.(js|styl)$/,
-                include: __dirname + '/src/*/components/',
-                loader: 'react-hot',
-            },
-        ],
-    },
-    stylus: {
-        use: [
-            poststylus([
-                'autoprefixer', // auto-prefix vendor styles
-            ]),
-        ],
-    },
-    plugins: [
-        // Instead of embedding css into the file where it is imported,
-        // extract all css into a single output file.
-        new ExtractTextPlugin(generateBuildFileName('css')),
-        // Generate an index.html file containing references to the newly created
-        //  js build file - build/build.COMMIT_HASH.min.js
-        // css build file - build/build.COMMIT_HASH.min.css
-        new HtmlWebpackPlugin({
-            template: __dirname + '/src/index.html',
-            inject: 'body', // inject js file reference into body element
-        }),
+  },
+  stylus: {
+    use: [
+      poststylus([
+        'autoprefixer', // auto-prefix vendor styles
+      ]),
     ],
+  },
+  plugins: [
+    // Instead of embedding css into the file where it is imported,
+    // extract all css into a single output file.
+    new ExtractTextPlugin(generateBuildFileName('css')),
+    // Generate an index.html file containing references to the newly created
+    //  js build file - build/build.COMMIT_HASH.min.js
+    // css build file - build/build.COMMIT_HASH.min.css
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html',
+      inject: 'body', // inject js file reference into body element
+    }),
+  ].concat(process.argv.includes('--notify') ? new WebpackBuildNotifierPlugin({
+    title: 'Golf',
+    sound: false,
+  }) : []),
 };
